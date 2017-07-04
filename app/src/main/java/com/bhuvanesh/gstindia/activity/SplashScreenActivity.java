c6bdfa83b0c64ad12fec344d73be49d2a479c0f9
karthikeyan19 <karthikeyana97@gmail.com>
2017-07-04 23:42:50 +0530

    icon 2

--- app/src/main/java/com/bhuvanesh/gstindia/activity/SplashScreenActivity.java
+++ app/src/main/java/com/bhuvanesh/gstindia/activity/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package com.bhuvanesh.gstindia.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +9,7 @@ import com.bhuvanesh.gstindia.BaseActivity;
 import com.bhuvanesh.gstindia.R;
 import com.bhuvanesh.gstindia.utils.GstLoggerUtil;
 
-public class SplashScreenActivity extends BaseActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,15 @@ public class SplashScreenActivity extends BaseActivity {
         setContentView(R.layout.activity_splash_screen);
 
         GstLoggerUtil.println("log this is a launcher activity");
+        int secdelay=3;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashScreenActivity.this,GstActivity.class);
+                startActivity(i);
+                finish();
+            }
+
+        },secdelay*1000);
     }
 }
